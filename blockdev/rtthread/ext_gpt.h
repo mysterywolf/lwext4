eda09cf84115fb7980b81ef388cff1573436ea77
linzhenxing-bit <linzhenxing@rt-thead.com>
2022-05-07 09:59:31 +0800

    添加lisence

--- blockdev/rtthread/ext_gpt.h
+++ blockdev/rtthread/ext_gpt.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author            Notes
+ * 2022-05-07     linzhenxing       add gpt
+ */
 #ifndef __EXT_GPT_H
 #define __EXT_GPT_H
 #include <stdint.h>
